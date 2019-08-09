7d7f521c59a6dceea9870c99e0baa86fadfc4d4f
League Student <league@iMac-15.attlocal.net>
2019-08-08 19:57:07 -0700

    CowTimer and Gridworld

---
+++ src/_06_gridworld/Gridworld_Runner.java
@@ -0,0 +1,32 @@
+package _06_gridworld;
+
+import java.awt.Color;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Location;
+
+public class Gridworld_Runner<pubic> {
+public void World() {
+			
+}
+public void show() {
+
+}
+public void add(Location loc, Actor occupant) {
+	
+}
+public void Bug() {
+	
+}
+pubic Bug(Color bugColor) {
+	return null;
+}
+}
+
+
+
+
+
+
+
+
